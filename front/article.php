00ca53723a1dc1b57ffd0ed6eadb1d9dbd07761e
NicolasM <nicolasmillet1@orange.fr>
2023-03-08 19:52:41 +0100

    [FIX] Lien du header

--- front/article.php
+++ front/article.php
@@ -29,7 +29,7 @@ $article = $sql->fetch(PDO::FETCH_ASSOC);
 <body>
 <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="Tpblog/front/blog.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <a href="/Tpblog/front/blog.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
             <span class="fs-4">Article : <?=$article['title']?></span>
         </a>
